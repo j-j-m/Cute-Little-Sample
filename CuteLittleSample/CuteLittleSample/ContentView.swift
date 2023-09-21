9c151969984b1c867cdd352b5e8c94b7047ea7a0
Jacob Martin <martinjaco@gmail.com>
2023-09-20 20:25:32 -0400

    adds base project

---
+++ CuteLittleSample/CuteLittleSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CuteLittleSample
+//
+//  Created by Jacob Martin on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CuteLittleSample/CuteLittleSample/CuteLittleSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuteLittleSampleApp.swift
+//  CuteLittleSample
+//
+//  Created by Jacob Martin on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CuteLittleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
